92cd529534ade0e28f9a16a6b620d9e1883d067f
Charles Lowell <cowboyd@frontside.io>
2019-05-03 17:57:58 -0600

    Remove defineChildren() function from Tree.
    defineChildren() is a microstates thing (that I'm not even sure is a
    really good idea)
    
    It doesn't relate to basic lenses and so doesn't need to be in this
    library.

--- src/tree.js
+++ src/tree.js
@@ -1,7 +1,5 @@
 import { type } from 'funcadelic';
 
-import CachedProperty from './cached-property';
-
 export const Tree = type(class {
   static get name() {
     return 'Tree';
@@ -14,16 +12,6 @@ export const Tree = type(class {
       return parent[key];
     }
   }
-
-  defineChildren(fn, parent) {
-    if (parent[Tree.symbol]) {
-      return this(parent).defineChildren(fn, parent);
-    } else {
-      for (let property of Object.keys(parent)) {
-        Object.defineProperty(parent, property, CachedProperty(property, () => fn(property, parent)));
-      }
-    }
-  }
 });
 
-export const { childAt, defineChildren } = Tree.prototype;
+export const { childAt } = Tree.prototype;
