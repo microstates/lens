f832072433a6f9023c540bbf0e77ab391ecdf217
Charles Lowell <cowboyd@frontside.io>
2019-05-03 17:57:58 -0600

    Remove defineChildren() function from Tree.
    defineChildren() is a microstates thing (that I'm not even sure is a
    really good idea)
    
    It doesn't relate to basic lenses and so doesn't need to be in this
    library.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,2 @@
 export { compose, view, over, set, Lens, transparent, At, Path } from "./lens";
-export { childAt, defineChildren, Tree } from "./tree";
-export { default as CachedProperty } from "./cached-property";
+export { childAt, Tree } from "./tree";
